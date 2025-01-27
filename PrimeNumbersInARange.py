6a85a8f4901530827d2247c6c970e2fbe10c39ba
MarinGG12 <thegoldenlotus1228@gmail.com>
2025-01-27 15:19:30 +0800

    Update

--- PrimeNumbersInARange.py
+++ PrimeNumbersInARange.py
@@ -4,6 +4,11 @@ start = int(input("Enter Start of range: "))
 end = int(input("Enter End of range: "))
 
 num = 0
-for num in range(start, end):
-    num += 1
-    print(num)
+for num in range(start, end +1):
+    if (num > 1):
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+    
