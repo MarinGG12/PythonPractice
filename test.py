eaac44346100dc2943a99d18e3bcf3dec4fd8dc8
MarinGG12 <thegoldenlotus1228@gmail.com>
2025-01-27 21:01:08 +0800

    Modified test.py to check my practice exercise for SoloLearn

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-a = 5
-b = True
-c = a > 8
-d = b or c 
+# take the number as input
+number = int(input("Enter a Number: "))
 
-print(a) #5
-print(b) #True
-print(c) #False
-print(d) #True
+#use a while loop for the countdown
+while number >= 0:
+    print(number)
+    number -= 1
